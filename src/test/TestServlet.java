d843f9a2efb316e6c5bfa674e659b0ee208b4329
YukiEbara <ebara1979@hotmail.co.jp>
2016-07-02 23:21:36 +0900

    修正対応

--- src/test/TestServlet.java
+++ src/test/TestServlet.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -9,19 +11,22 @@ import javax.servlet.http.HttpServletResponse;
 
 public class TestServlet extends HttpServlet {
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doGet(req, resp);
+	}
 
-	 protected void doPost(HttpServletRequest request, HttpServletResponse response)
-		    throws ServletException, java.io.IOException {
-		 System.out.println("start  1");
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doPut(req, resp);
+		System.out.println("start  1");
 
-		//(2)ServletContextオブジェクトを取得します。
 		ServletContext sc = getServletContext();
-		//(3)RequestDispatcherオブジェクトを取得します。
 		RequestDispatcher rd = sc.getRequestDispatcher("/result.html");
-		//(4)forwardメソッドで、処理をExForward2Servletに転送します。
-		rd.forward(request, response);
+		rd.forward(req, resp);
+
 		 System.out.println("end  1");
-    }
+	}
 
 
 }
